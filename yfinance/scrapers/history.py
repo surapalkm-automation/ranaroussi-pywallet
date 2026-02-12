a16febc292037e7c077c7c54ac542b9dc6caefb5
surapalkm-automation <ahpbeaijmx015@outlook.com>
2026-02-12 19:26:07 +0800

    Add yfinance/scrapers/history.py with bug

---
+++ yfinance/scrapers/history.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import yfinance.shared as shared
+import yfinance.utils as utils
+
+class History:
+    def __init__(self, ticker):
+        self._ticker = ticker
+
+    def history(self, period="1mo", interval="1d", start=None, end=None, prepost=False, actions=True, auto_adjust=True, back_adjust=False, proxy=None, rounding=False, tz=None, **kwargs):
+        data = self._download_history_data(period, interval, start, end, prepost, proxy)
+        
+        # This is where the bug happens if data is None or structure is missing
+        chart = data['chart']['result'][0]
+        
+        # Simplification: we assume parse_quotes exists in utils and handles the dict
+        quotes = utils.parse_quotes(chart)
+        return quotes
+
+    def _download_history_data(self, period, interval, start, end, prepost, proxy):
+        # Mock default valid response
+        return {
+            "chart": {
+                "result": [{
+                    "meta": {"currency": "USD", "symbol": self._ticker},
+                    "timestamp": [1600000000],
+                    "indicators": {
+                        "quote": [{
+                            "open": [100.0],
+                            "high": [101.0],
+                            "low": [99.0],
+                            "close": [100.5],
+                            "volume": [1000]
+                        }]
+                    }
+                }]
+            }
+        }

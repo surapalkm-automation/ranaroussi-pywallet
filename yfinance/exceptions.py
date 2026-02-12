eb6e3ac22693b2f515fc071762d635e40138330e
surapalkm-automation <ahpbeaijmx015@outlook.com>
2026-02-12 19:24:06 +0800

    Add exceptions.py

---
+++ yfinance/exceptions.py
@@ -0,0 +1,6 @@
+class YFException(Exception): pass
+class YFDataException(Exception): pass
+class YFInvalidPeriodError(Exception): pass
+class YFPricesMissingError(Exception): pass
+class YFRateLimitError(Exception): pass
+class YFTzMissingError(Exception): pass
